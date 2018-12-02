919a6b56b560674fdaba0ee9ab6da14728e984c2
Theo Daly <theo.daly@outlook.com>
2018-12-02 17:24:51 -0600

    functioning app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,14 @@
 
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var bamazon = require("./bamazonItems.sql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+
+const connection = mysql.createConnection({
     host: "localhost",
 
     port: 3306,
     user: "root",
-    password: "",
+    password: "Hotdog18!",
     database: "bamazon",
 });
 
@@ -21,58 +21,57 @@ function buyItem() {
     connection.query('SELECT * FROM Products', function (err, res) {
 
 
-        for (var i = 0; i < res.length; i++) {
-            console.log('Item: ' + res[i].ItemName + ' /n Price: ' + res[i].Price + ' /n Stock: ' + res[i].StockQuantity);
+        for (const i = 0; i < res.length; i++) {
+            console.log('Item: ' + res[i].ItemName + ' \n Price: ' + res[i].Price + ' \n Stock: ' + res[i].StockQuantity);
         }
-
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    message: "What would you like to buy?",
-                    choices: function (value) {
-                        var choiceArray = [];
-                        for (var i = 0; i < res.length; i++) {
-                            choiceArray.push(res[i].ItemName);
-                        }
-                        return choiceArray;
+        //these prompts look weird? figure out the line breaks
+        inquirer.prompt([
+            {
+                name: "choice",
+                type: "rawlist",
+                message: "What would you like to buy?",
+                choices: function (value) {
+                    const choiceArray = [];
+                    for (const i = 0; i < res.length; i++) {
+                        choiceArray.push(res[i].ItemName);
                     }
-                },
-                {
-                    name: "quantity",
-                    type: "input",
-                    message: "How many would you like to buy?",
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
-                        }
-                        return false;
-                    }
+                    return choiceArray;
                 }
-            ])
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
             .then(function (answer) {
-                for (var i = 0; i < res.length; i++) {
+                for (const i = 0; i < res.length; i++) {
                     if (res[i].ItemName == answer.choice) {
-                        var chosenItem = res[i];
+                        const chosenItem = res[i];
                     }
                 }
-                var updateStock = +chosenItem.StockQuantity - +answer.quantity;
+                const updateStock = +chosenItem.StockQuantity - +answer.quantity;
 
                 if (chosenItem.StockQuantity < +answer.quantity) {
                     console.log("Insufficient quantity!");
 
-                    again();
+                    buyAgain();
                 }
 
                 else {
                     connection.query("UPDATE Items SET ? WHERE ?", [{ StockQuantity: updateStock }, { ItemId: chosenItem.ItemId }], function (err, res) {
                         console.log("Purchase successful!");
 
-                        var Total = +answer.quantity * chosenItem.Price.toFixed(2);
+                        const Total = +answer.quantity * chosenItem.Price.toFixed(2);
                         console.log("Your total is $" + Total);
 
-                        again();
+                        buyAgain();
                     });
                 }
             });
@@ -80,10 +79,9 @@ function buyItem() {
     });
 
 }
-//ok there is an error in here on line 80 somewhere?? I don't know where it is!//
-//I've compared all these class activities and can't figure out the problem :(//
 
-function again() {
+
+function buyAgain() {
     inquirer.prompt({
         name: "repurchase",
         type: "list",
